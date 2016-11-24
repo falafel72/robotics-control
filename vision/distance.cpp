a5b76f4c3e40910451e69a3d6600ccbbb7859741
falafel72 <ksh717@student.cis.edu.hk>
2016-11-24 15:38:54 +0800

    Got disparity map working for both C++ and Python

--- vision/distance.cpp
+++ vision/distance.cpp
@@ -8,38 +8,82 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <iostream.h>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <iostream>
+#include <stdio.h>
 
 
 using namespace cv;
+using namespace std;
 
 int main(int args, char** argv) {
 
 	//get two camera feeds
-	cv::VideoCapture camera1, camera2;
+	/*cv::VideoCapture camera1, camera2;
 	camera1.open(1);
 	camera2.open(2);
 
 	if(!camera1.isOpened() || !camera2.isOpened()) {
-		std::err << "ERROR: Unable to access camera!" << endl;
+		cerr << "ERROR: Unable to access camera!" << endl;
 	}
+*/
+	//while(waitKey(20) != 'q') {
+		Mat frameL,frameR;
+		frameL = imread("img/1.jpg",0);
+		frameR = imread("img/2.jpg",0);
 
-	while(waitKey(20) != 'q') {
-		Mat3b frame1,frame2;
-		camera1 >> frameL;
-		camera2 >> frameR;
+		//resize(frameL,frameL,Size(512,384));
+		//resize(frameR,frameR,Size(512,384));
+		/*camera1 >> frameL;
+		camera2 >> frameR;*/
 
 		imshow("Cam 1", frameL);
 		imshow("Cam 2", frameR);
 
 		//calculate disparity map between feeds
+		int ndisparities = 16*7;
+		int SADWindowSize = 21;
 
+		
+
+
+		Mat disparity16S = Mat(frameL.rows,frameL.cols,CV_16S);
+		Mat disparity8U = Mat( frameL.rows, frameR.cols, CV_8UC1 );
+
+		// StereoBM is an abstract class in OpenCV3 for some reason
+		//Ptr<StereoBM> stereo = StereoBM::create(ndisparities,SADWindowSize);
+		//stereo->compute(frameL,frameR,disparity16S);
+
+		// This is the way to go using OpenCV2
+		StereoBM sbm;
+		sbm.state->SADWindowSize = 9;
+		sbm.state->numberOfDisparities = 112;
+		sbm.state->preFilterSize = 5;
+		sbm.state->preFilterCap = 61;
+		sbm.state->minDisparity = -39;
+		sbm.state->textureThreshold = 507;
+		sbm.state->uniquenessRatio = 0;
+		sbm.state->speckleWindowSize = 0;
+		sbm.state->speckleRange = 8;
+		sbm.state->disp12MaxDiff = 1;
+
+		sbm(frameL,frameR,disparity16S);
+
+		double min, max;
+		minMaxLoc(disparity16S,&min,&max);
+		printf("Min disp: %f Max value: %f \n", min, max);
+
+		//convert signed to unsigned to be displayed
+		disparity16S.convertTo(disparity8U,CV_8UC1,255/(max-min));
+		imshow("disparity", disparity8U);
+
+		waitKey();
 
 		//print distance at the middle of the frame
 
 
-	}
-
+	//}
+	//cout << CV_VERSION << endl;
 
 
 	return 0;
--- vision/distance.py
+++ vision/distance.py
@@ -1,21 +1,41 @@
+'''
+	Receives stereo camera feed and calculates disparity map. 
+	Returns distance value in the middle of the screen. 
+
+'''
+# Import libraries
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+# Initialise cameras
+'''
 capL = cv2.VideoCapture(1)
 capR = cv2.VideoCapture(2)
+'''
 
+# Repeat until prompted
 while cv2.waitKey(20) != ord('q'): 
+	'''
 	retL, frameL = capL.read()
 	retR, frameR = capR.read()
+	'''
+	frameL = cv2.imread('img/1.jpg',0)
+	frameR = cv2.imread('img/2.jpg',0)
 
-	stereo = cv2.createStereoBM(numDisparities = 16, blockSize = 15)
+	# calculate disparity bitmap
+	stereo = cv2.StereoBM(0,ndisparities = 16, SADWindowSize = 15)
 	disparity = stereo.compute(frameL,frameR)
+	disparity8U = cv2.convertScaleAbs(disparity)
 
-	imshow(frameL)
-	imshow(frameR)
-	imshow(disparity,'gray')
+	cv2.imshow('left',frameL)
+	cv2.imshow('right',frameR)
+	cv2.imshow('gray',disparity8U)
 
+	# get value in the middle of the bitmap
+
+
+# destroy all frames when done
 capL.release()
 capR.release()
 cv2.destroyAllWindows()
